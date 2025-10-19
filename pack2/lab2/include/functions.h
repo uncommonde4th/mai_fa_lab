b01fd5c635c6b99a0c1ef4a5a7003e868cf57b80
George Brezhnev <uncommonde4th@gmail.com>
2025-10-19 20:28:39 +0300

    p2l2 v0.1

---
+++ pack2/lab2/include/functions.h
@@ -0,0 +1,18 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <stdio.h>
+
+void *memchr(const void *str, int c, size_t n);
+int memcmp(const void *str1, const void *str2, size_t n);
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *str, int c, size_t n);
+char *strncat(char *dest, const char *src, size_t n);
+char *strchr(const char *str, int c);
+int strncmp(const char *str1, const char *str2, size_t n);
+char *strncpy(char *dest, const char *src, size_t n);
+size_t strcspn(const char *str1, const char *str2);
+char *strerror(int errnum);
+
+#endif
+
---
+++ pack2/lab2/src/functions.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include "../include/functions.h"
+
+// Выполняет поиск первого вхождения символа c (беззнаковый тип) в первых n байтах строки, на которую указывает аргумент str.
+void *memchr(const void *str, int c, size_t n) {
+
+}
+
+int memcmp(const void *str1, const void *str2, size_t n) {
+
+}
+
+void *memcpy(void *dest, const void *src, size_t n) {
+
+}
+
+void *memset(void *str, int c, size_t n) {
+
+}
+
+char *strncat(char *dest, const char *src, size_t n) {
+
+}
+
+char *strchr(const char *str, int c) {
+
+}
+
+int strncmp(const char *str1, const char *str2, size_t n) {
+
+}
+
+char *strncpy(char *dest, const char *src, size_t n) {
+
+}
+
+size_t strcspn(const char *str1, const char *str2) {
+
+}
+
+char *strerror(int errnum) {
+
+}
+
+size_t strlen(const char *str) {
+
+}
+
+char *strpbrk(const char *str1, const char *str2) {
+
+}
+
+char *strrchr(const char *str, int c) {
+
+}
+
+char *strstr(const char *haystack, const char *needle) {
+
+}
+
+char *strtok(char *str, const char *delim) {
+
+}
